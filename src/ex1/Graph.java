08e29e4eacabb50b24de85497895c93034d39333
Alexandre <bocketo@gmail.com>
2018-01-15 16:17:44 +0100

    interface Graph

---
+++ src/ex1/Graph.java
@@ -0,0 +1,11 @@
+package ex1;
+
+import java.util.Set;
+
+public interface Graph<V> {
+	
+	boolean addEdge(V fromVertex, V toVertex);
+	boolean addVertex(V vertex);
+	Set<V> getChildren(V vertex);
+	
+}
