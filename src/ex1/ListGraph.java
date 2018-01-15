1b70fc4d47e8662d52eb2b10447b37f6c51bd11c
Vincent Jacob <vincent.jacob@edu.ece.fr>
2018-01-15 16:45:24 +0100

    existingVertex

--- src/ex1/ListGraph.java
+++ src/ex1/ListGraph.java
@@ -7,15 +7,23 @@ public class ListGraph<V> implements Graph<V>{
 
 	private ArrayList<ArrayList<V>> myGraph;
 
+	public ListGraph() {
+		myGraph = new ArrayList<ArrayList<V>>();
+	}
+	
 	@Override
 	public boolean addEdge(V fromVertex, V toVertex) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	private int existingVertex() {
+		return 0;
+	}
+	
 	@Override
 	public boolean addVertex(V vertex) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
