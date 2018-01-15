6c3632cb11f906acfa96df9ea6f8727423a7526b
Alexandre <bocketo@gmail.com>
2018-01-15 16:22:35 +0100

    class ListGraph

---
+++ src/ex1/ListGraph.java
@@ -0,0 +1,27 @@
+package ex1;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class ListGraph<V> implements Graph{
+
+	private ArrayList<ArrayList<V>> myAdjList;
+
+	@Override
+	public boolean addEdge(Object fromVertex, Object toVertex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addVertex(Object vertex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set getChildren(Object vertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
